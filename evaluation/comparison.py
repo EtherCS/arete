939e865911eee7a14901293fb7e582fdc77f0cc1
Ether <antfinancialyujian@gmail.com>
2023-06-03 10:26:53 -0400

    update comparison

--- evaluation/comparison.py
+++ evaluation/comparison.py
@@ -26,7 +26,8 @@ def minCSize(n, s, f, k):
         h = math.ceil((1 - f) * m) # we want at least h honest parties to not violate corruption threshold
         if pFail(n, s, m, h, pMax) <= pMax :
             return m
-        
+
+# return the probability of compromising liveness with a given shard size
 def pFailWithShardSize(n, s, m, h):
     p = 0
     denom = scipy.special.comb(n, m, exact = True) # compute n choose m as exact integer
@@ -45,37 +46,44 @@ print("Total nodes:", n, " ", "Security parameter:", k)
 # print("protocol\t s\t fS\t fL\t m\t")
 print("protocol\t s\t fS\t fL\t m\t liveness_ratio")
 
-print("OmniLedger\t 25%\t 33.3%\t 33.3%\t", minCSize(n, n*Fraction(25, 100), Fraction(1, 3), k), "\t", "1-2^(-60)")
-print("RapidChain\t 33.3%\t 49%\t 49%\t", minCSize(n, n*Fraction(1, 3), Fraction(49, 100), k), "\t", "1-2^(-60)")
-print("AHL\t\t 30%\t 49%\t 49%\t", minCSize(n, n*Fraction(3, 10), Fraction(49, 100), k), "\t", "1-2^(-60)")
-print("Pyramid\t\t 12.5%\t 33.3%\t 33.3%\t", minCSize(n, n*Fraction(1, 8), Fraction(1, 3), k), "\t", "1-2^(-60)")
-print("RIVET\t\t 33.3%\t 49%\t 49%\t", minCSize(n, n*Fraction(1, 3), Fraction(49, 100), k), "\t", "1-2^(-60)")
+print("OmniLedger\t 25%\t 33.3%\t 33.3%\t", minCSize(n, n*Fraction(25, 100), Fraction(1, 3), k), "\t", "1-2^("+str(k)+")")
+print("RapidChain\t 33.3%\t 49%\t 49%\t", minCSize(n, n*Fraction(1, 3), Fraction(49, 100), k), "\t", "1-2^("+str(k)+")")
+print("AHL\t\t 30%\t 49%\t 49%\t", minCSize(n, n*Fraction(3, 10), Fraction(49, 100), k), "\t", "1-2^("+str(k)+")")
+print("Pyramid\t\t 12.5%\t 33.3%\t 33.3%\t", minCSize(n, n*Fraction(1, 8), Fraction(1, 3), k), "\t", "1-2^("+str(k)+")")
+print("RIVET\t\t 33.3%\t 49%\t 49%\t", minCSize(n, n*Fraction(1, 3), Fraction(49, 100), k), "\t", "1-2^("+str(k)+")")
 
-f_s =39
-f_l = 30
-m_min = minCSize(n, n*Fraction(30, 100), Fraction(f_s, 100), k)
-print("GEARBOX\t\t 30%\t 39%\t 30%\t", minCSize(n, n*Fraction(3, 10), Fraction(39, 100), k), "\t", float(1-pFailWithShardSize(n, s, m_min, int(m_min*Fraction(100-f_l, 100)))))
+# GEARBOX
+crps = range(0 , 34 , 1)
+print(" ")
+s_n = 25    # consider s=25%
+for crp in crps:
+    f_l = crp 
+    f_s = 100-2*f_l-1
+    m_min = minCSize(n, n*Fraction(s_n, 100), Fraction(f_s, 100), k)
+    print("GEARBOX\t\t", str(s_n) + "%\t", str(f_s) + "%\t", str(f_l) + "%\t", m_min, "\t", float(1-pFailWithShardSize(n, n*Fraction(s_n, 100), m_min, int(m_min*Fraction(100-f_l, 100)))))
 
-f_s =39
-f_l = 30
-m_min = minCSize(n, n*Fraction(25, 100), Fraction(f_s, 100), k)
-print("GEARBOX\t\t 25%\t 39%\t 30%\t", minCSize(n, n*Fraction(25, 100), Fraction(39, 100), k), "\t", float(1-pFailWithShardSize(n, s, m_min, int(m_min*Fraction(100-f_l, 100)))))
+print(" ")
+s_n = 30    # consider s=30%
+for crp in crps:
+    f_l = crp 
+    f_s = 100-2*f_l-1
+    m_min = minCSize(n, n*Fraction(s_n, 100), Fraction(f_s, 100), k)
+    print("GEARBOX\t\t", str(s_n) + "%\t", str(f_s) + "%\t", str(f_l) + "%\t", m_min, "\t", float(1-pFailWithShardSize(n, n*Fraction(s_n, 100), m_min, int(m_min*Fraction(100-f_l, 100)))))
 
-f_s =49
-f_l = 25
-m_min = minCSize(n, n*Fraction(25, 100), Fraction(f_s, 100), k)
-print("GEARBOX\t\t 25%\t 49%\t 25%\t", minCSize(n, n*Fraction(25, 100), Fraction(49, 100), k), "\t", float(1-pFailWithShardSize(n, s, m_min, int(m_min*Fraction(100-f_l, 100)))))
+# ARETE
+crps = range(99 , 49 , -1)
+print(" ")
+s_n = 25    # consider s=25%
+for crp in crps:
+    f_s = crp 
+    f_l = 100-f_s-1
+    m_min = minCSize(n, n*Fraction(s_n, 100), Fraction(f_s, 100), k)
+    print("ARETE\t\t", str(s_n) + "%\t", str(f_s) + "%\t", str(f_l) + "%\t", m_min, "\t", float(1-pFailWithShardSize(n, n*Fraction(s_n, 100), m_min, int(m_min*Fraction(100-f_l, 100)))))
 
-
-f_s =59
-f_l = 40
-m_min = minCSize(n, n*Fraction(25, 100), Fraction(f_s, 100), k)  # s = 25%, f_L=40%
-print("ARETE\t\t 25%\t 59%\t 40%\t", m_min, "\t", float(1-pFailWithShardSize(n, s, m_min, int(m_min*Fraction(100-f_l, 100)))))
-
-m_min = minCSize(n, n*Fraction(30, 100), Fraction(f_s, 100), k)  # s = 30%, f_L=40%
-print("ARETE\t\t 30%\t 59%\t 40%\t", m_min, "\t", float(1-pFailWithShardSize(n, s, m_min, int(m_min*Fraction(100-f_l, 100)))))
-
-f_s =50
-f_l = 49
-m_min = minCSize(n, n*Fraction(25, 100), Fraction(f_s, 100), k)  # s = 25%, f_L=49%
-print("ARETE\t\t 25%\t 50%\t 49%\t", m_min, "\t", float(1-pFailWithShardSize(n, s, m_min, int(m_min*Fraction(100-f_l, 100)))))
+print(" ")
+s_n = 30    # consider s=30%
+for crp in crps:
+    f_s = crp 
+    f_l = 100-f_s-1
+    m_min = minCSize(n, n*Fraction(s_n, 100), Fraction(f_s, 100), k)
+    print("ARETE\t\t", str(s_n) + "%\t", str(f_s) + "%\t", str(f_l) + "%\t", m_min, "\t", float(1-pFailWithShardSize(n, n*Fraction(s_n, 100), m_min, int(m_min*Fraction(100-f_l, 100)))))
